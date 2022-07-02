b34616fd4285288eafd76434f97b0942b439a2dd
TolIk <anatoliyyushkevich@gmail.com>
2022-07-02 13:54:16 +0300

    commit

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print('Hello world!')
+def find_beautiful_numbers(num):
+
+    len_num = len(str(num))
+    counter = 0
+    str_num = str(num)
+
+    for i in range(len_num):
+        if (i + 1) % 2 == 1:
+            if int(str_num[i]) % 2 == 1:
+                counter += 1
+        if (i + 1) % 2 == 0:
+            if int(str_num[i]) % 2 == 0:
+                counter += 1
+
+    if counter == len_num:
+        return True
+    else:
+        return False
+
+
+
+
+
+
+numb = int(input())
+print(find_beautiful_numbers(numb))
