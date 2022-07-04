1421b34b785a8dcc7bbb160573f030f36c165983
TolIk <anatoliyyushkevich@gmail.com>
2022-07-04 23:12:36 +0300

    commit

--- tasks.py
+++ tasks.py
@@ -7,13 +7,15 @@ def count_seanses(len_of_working, time_from_last_cleaning, len_of_seans, len_of_
             count_of_time += len_of_seans
             count_of_seans += 1
             count_of_etaretions += len_of_seans
+            if count_of_time >= len_of_working:
+                break
+
         count_of_etaretions = 0
         count_of_time += len_of_cleaning
-        print(count_of_time)
     return f'При данных условиях, за {len_of_working} минут, в день будет проводиться {count_of_seans} сенсов.'
 
 
-
+# now I'm absaluteli sure that thise works:)
 
 
 print(count_seanses(int(input()), int(input()), int(input()), int(input())))
