d7343c94703bc964f736321d1884b1e4d1e03d59
TolIk <anatoliyyushkevich@gmail.com>
2022-07-06 22:01:44 +0300

    commit

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+
 def count_seanses(len_of_working, time_from_last_cleaning, len_of_seans, len_of_cleaning):
     count_of_time = 0
     count_of_etaretions = 0
@@ -12,10 +13,20 @@ def count_seanses(len_of_working, time_from_last_cleaning, len_of_seans, len_of_
 
         count_of_etaretions = 0
         count_of_time += len_of_cleaning
+        if count_of_time + len_of_seans > len_of_working:
+            break
     return f'При данных условиях, за {len_of_working} минут, в день будет проводиться {count_of_seans} сенсов.'
 
 
-# now I'm absaluteli sure that thise works:)
+# now I'm absolutely sure that that works:)
 
 
-print(count_seanses(int(input()), int(input()), int(input()), int(input())))
+text_file = open('texsst.txt', 'r')
+len_of_working = int(text_file.readline())
+time_from_last_cleaning = int(text_file.readline())
+len_of_seans = int(text_file.readline())
+len_of_cleaning = int(text_file.readline())
+
+
+
+print(count_seanses(len_of_working, time_from_last_cleaning, len_of_seans, len_of_cleaning))
--- test.py
+++ test.py
@@ -1,32 +1,6 @@
-
-def find_beautiful_numbers(num):
-
-    len_num = len(str(num))
-    counter = 0
-    str_num = str(num)
-
-    for i in range(len_num):
-        if (i + 1) % 2 == 1:
-            if int(str_num[i]) % 2 == 1:
-                counter += 1
-        if (i + 1) % 2 == 0:
-            if int(str_num[i]) % 2 == 0:
-                counter += 1
-
-    if counter == len_num:
-        return True
-    else:
-        return False
-
-
-numb = int(input())
-counter2 = 0
-if find_beautiful_numbers(numb) == True:
-    counter2 += 1
-
-
-while numb != 0:
-    numb = int(input())
-    if find_beautiful_numbers(numb) == True:
-        counter2 += 1
-print(counter2)
+text_file = open('texsst.txt', 'r')
+len_of_working = int(text_file.readline())
+time_from_last_cleaning = int(text_file.readline())
+len_of_seans = int(text_file.readline())
+len_of_cleaning = int(text_file.readline())
+print(len_of_working, time_from_last_cleaning, len_of_seans, len_of_cleaning)
