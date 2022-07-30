450d8e11ccc3142d3117df07d405134a21c06cc9
TolIk <anatoliyyushkevich@gmail.com>
2022-07-30 15:52:33 +0300

    commit

--- task_3.py
+++ task_3.py
@@ -2,36 +2,30 @@ text_file = open('texsst.txt')
 key = text_file.readline()
 text = text_file.readline()
 result = ''
-counter = 0
-let = ''
+key_letter_index = 0
+
 while text != '':
-    if text != '':
-        for s in text:
-            if 64 < ord(s) < 123:
-                if counter + 1 >= len(key):
-                    counter = 0
-                let = key[counter]
-                if 64 < ord(s) < 91:
-                    s = s.lower()
-                    if (ord(let) - 96) + (ord(s) - 96) <= 26:
-                        result += chr(((ord(let) - 96) + (ord(s) - 96)) + 96).upper()
-                    elif (ord(let) - 96) + (ord(s) - 96) > 26:
-                        result += chr(((ord(let) - 96) + (ord(s) - 96) - 26) + 96).upper()
-                else:
-                    if counter + 1 >= len(key):
-                        counter = 0
-                    let = key[counter]
-                    if ord(let) - 96 > 0 and ord(s) - 96 > 0:
-                        if (ord(let) - 96) + (ord(s) - 96) <= 26:
-                            result += chr(((ord(let) - 96) + (ord(s) - 96)) + 96)
-                        elif (ord(let) - 96) + (ord(s) - 96) > 26:
-                            result += chr(((ord(let) - 96) + (ord(s) - 96) - 26) + 96)
-                let = ''
-                counter += 1
-            else:
-                result += s
-        text = text_file.readline()
+    for text_letter in text:
+        if ord(text_letter) <= 64 or ord(text_letter) > 123:
+            result += text_letter
+            continue
 
+        if key_letter_index + 1 >= len(key):
+            key_letter_index = 0
+        key_letter = key[key_letter_index]
 
+        if 64 < ord(text_letter) < 91:
+            base_index = ord('A') - 1
+        else:
+            base_index = ord('a') - 1
+
+        if (ord(key_letter) - base_index) + (ord(text_letter) - base_index) <= 26:
+            result += chr(((ord(key_letter) - base_index) + (ord(text_letter) - base_index)) + base_index)
+        elif (ord(key_letter) - base_index) + (ord(text_letter) - base_index) > 26:
+            result += chr(((ord(key_letter) - base_index) + (ord(text_letter) - base_index) - 26) + base_index)
+
+        key_letter_index += 1
+
+    text = text_file.readline()
 
 print(result)
